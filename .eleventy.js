60f958269b4a0810dbf59b99337f4ed3b4d957c3
Brendan Sparrow <btsparrow@hotmail.co.uk>
2019-07-05 13:18:05 +0100

    Configure Eleventy

---
+++ .eleventy.js
@@ -0,0 +1,37 @@
+const CleanCSS = require('clean-css')
+
+module.exports = function (eleventyConfig) {
+  // Filters
+  eleventyConfig.addFilter('cssmin', function (code) {
+    return new CleanCSS({}).minify(code).styles
+  })
+
+  // Aliases are in relation to the _includes folder
+  eleventyConfig.addLayoutAlias('base', 'layouts/base.liquid')
+
+  // Passtrhough copy
+  eleventyConfig.addPassthroughCopy('src/fonts')
+  eleventyConfig.addPassthroughCopy('src/images')
+  eleventyConfig.addPassthroughCopy('src/js')
+
+  eleventyConfig.addPassthroughCopy('src/favicon.ico')
+  eleventyConfig.addPassthroughCopy('src/favicon-16x16.png')
+  eleventyConfig.addPassthroughCopy('src/favicon-32x32.png')
+  eleventyConfig.addPassthroughCopy('src/mstile-150x150.png')
+  eleventyConfig.addPassthroughCopy('src/safari-pinned-tab.svg')
+  eleventyConfig.addPassthroughCopy('src/apple-touch-icon.png')
+  eleventyConfig.addPassthroughCopy('src/android-chrome-192x192.png')
+  eleventyConfig.addPassthroughCopy('src/android-chrome-512x512.png')
+  eleventyConfig.addPassthroughCopy('src/browserconfig.xml')
+  eleventyConfig.addPassthroughCopy('src/site.webmanifest')
+
+  return {
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
+    dir: {
+      includes: '_includes', // Equivalent to Jekyll's includes property
+      input: './src',      // Equivalent to Jekyll's source property
+      output: './dist' // Equivalent to Jekyll's destination property
+    },
+    passthroughFileCopy: true
+  }
+}
